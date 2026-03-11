628e59faf6aa7803cf2f7e26fda59784f3983bcf
tijaraproai <tijaraproapp@gmail.com>
2026-03-11 11:44:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("ForYou Shop Ready 🚀");
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+document.getElementById("productForm")?.addEventListener("submit", function(e){
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let price = document.getElementById("price").value;
+    let image = document.getElementById("image").value;
+    let description = document.getElementById("description").value;
+    
+    let newProduct = {title, price, image, description};
+    products.push(newProduct);
+    localStorage.setItem("products", JSON.stringify(products));
+    alert("تم رفع المنتج بنجاح!");
+    this.reset();
+});
